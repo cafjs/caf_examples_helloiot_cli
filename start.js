d61d59368559deeaa5ce3664547f8c14f6879820
Antonio Lain <antlai@cafjs.com>
2013-02-11 23:04:23 -0800

    Add proxy

--- start.js
+++ start.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 var client = require('./index.js');
-if (process.argv.length != 3) {
-    console.log('Error: Wrong number of arguments: start.js url ' +
-                ' where url is, e.g.: http://helloiot.cafjs.com/iot/<device_id>');
-} else {
+if (process.argv.length === 3) {
     var config = {url: process.argv[2]};
     client.start(config);
+} else if (process.argv.length === 4) {
+    var config = {url: process.argv[2], proxy: process.argv[3]};
+    client.start(config);
+} else {
+    console.log('Error: Wrong number of arguments: start.js url '+
+                '<optional_proxy> where url is, e.g.:' +
+                'http://helloiot.cafjs.com/iot/<device_id>');
 }
